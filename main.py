38821d15de22afc8945f899e9518d6309d2eaab0
6MillionBees <BoettcherAr321@northwested.org>
2024-09-13 09:59:27 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,43 +2,46 @@
 # 9/13/24
 # string numeric input starter main
 
+
 # Task 1
-# Comment
-# Comment
+# Use the input( ) function to prompt the user to enter his/her first name and the name of the program they attend at Career Tech.
+# Assign the user's responses to variables
+# Use concatenation to build an output string that displays the user's answers
+# Please use correct capitalization, spelling, and punctuation in your code
 
 
 
 
 # Task 2
-# Comment
-# Comment
+# prompt the user to enter their current age
+# Assign their response to a variable age
+# On the next line of code, use the int( ) function to convert the user's response to an integer (a number that doesn't have a decimal point)
+# Display a sentence that contains the user's current age
+# Challenge: Have Python add 10 years to the user's current age; then display a sentence that says what the user's age will be ten years from now
 
 
 
 
 # Task 3
-# Comment
-# Comment
-
+# Using the input( ) and int ( ) functions in a single line of code, prompt the user to enter the number of people in his/her family
+# Use concatenation to display a sentence that says how many people are in the user's immediate family
+# HINT: The Python str(  ) function might come in handy here!
+# On the next line of code, use an f-string to display a sentence that says how many people are in the user's immediate family
 
 
 
 # Task 4
-# Comment
-# Comment
+# Using the input( ) and float ( ) functions in a single line of code, prompt the user to enter their height in inches (so 60 for 5'10" tall)
+# Write a formula that tells Python how to do the math to convert inches into centimeters
+# HINT: Define a constant called CONVERSION_FACTOR and assign it the value 2.54
+# Use concatenation to display a sentence that says how tall the user is in inches AND in centimeters
+# HINT: The Python str(  ) function might come in handy here!
+# Also show you know how to use an f-string to display the user's height in both inches and centimeters
 
 
 
 
 
 # Task 5
-# Comment
-# Comment
-
-
-
-
-
-# Part 5
 # Comment
 # Comment
